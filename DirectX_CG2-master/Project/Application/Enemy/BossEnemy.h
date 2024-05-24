ba2fbed9f443fbf5d56e801a50888bc6c336eafa
yamashitatubasa <k021g1247@g.neec.ac.jp>
2024-05-24 12:58:03 +0900

    ボスの位置の修正

--- DirectX_CG2-master/Project/Application/Enemy/BossEnemy.h
+++ DirectX_CG2-master/Project/Application/Enemy/BossEnemy.h
@@ -44,7 +44,7 @@ private:
 	std::list<std::unique_ptr<BossEnemyBullet>> bossEnemyBullet_;
 
 	// ボスの座標、スケール、回転、移動量
-	DirectX::XMFLOAT3 position_ = { -10.0f,-10.0f,0.0f };
+	DirectX::XMFLOAT3 position_ = { 10.0f,-10.0f,0.0f };
 	DirectX::XMFLOAT3 scale_ = { 5.0f,5.0f,5.0f };
 	DirectX::XMFLOAT3 rotation_ = { 0.0f,0.0f,0.0f };
 	DirectX::XMFLOAT3 move_ = { 0.5f,0.0f,0.0f };
