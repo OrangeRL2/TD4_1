5e44b024cf31795ddd1da8405e21019e5a98ee89
yamashitatubasa <k021g1247@g.neec.ac.jp>
2024-05-29 15:44:34 +0900

    ボスの移動処理の修正

--- DirectX_CG2-master/Project/Application/Enemy/BossEnemy.cpp
+++ DirectX_CG2-master/Project/Application/Enemy/BossEnemy.cpp
@@ -16,10 +16,14 @@ void BossEnemy::Initialize()
 	bossEnemyObj_->SetPosition(position_);
 	bossEnemyObj_->SetScale(scale_);
 	bossEnemyObj_->SetRotation(rotation_);
+
+	speed_ = 0.0f;
 }
 
 void BossEnemy::Update()
 {
+	Move();
+	
 	bossEnemyObj_->Update();
 }
 
@@ -38,6 +42,34 @@ void BossEnemy::Attack()
 void BossEnemy::Move()
 {
 	// ボスの移動
+	move_.x = speed_ + speedBoost_;
+	if (speed_ > speedLim_) {
+		speed_ = speedLim_;
+	}
+	if (speed_ < speedLim_)
+	{
+		speed_ += 0.001f;
+	}
+	//動き
+	if (OogamiEngine::Input::GetInstance()->PushKey(DIK_D)) {
+		if (turnSpeed_ < 10.0f)
+		{
+			turnSpeed_ += 1.0f;
+		}
+		position_.y += gravity_;
+	}
+	//上に泳いでない時に下に動く
+	else if (position_.y >= -moveLim_) {
+		if (turnSpeed_ > -10.0f)
+		{
+			turnSpeed_ -= 1.0f;
+		}
+		position_.y -= gravity_;
+	}
+	
 	position_.x += move_.x;
+	position_.y += move_.y;
+	position_.z += move_.z;
 	bossEnemyObj_->SetPosition(position_);
+	bossEnemyObj_->SetRotation(rotation_);
 }
