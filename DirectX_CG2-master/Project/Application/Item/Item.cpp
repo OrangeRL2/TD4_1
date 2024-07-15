1925689e12246363379492a3bb2d13e12d553052
tominagayuuki <k021g1421@g.neec.ac.jp>
2024-07-15 14:41:09 +0900

    アイテムの処理の調整

--- DirectX_CG2-master/Project/Application/Item/Item.cpp
+++ DirectX_CG2-master/Project/Application/Item/Item.cpp
@@ -76,9 +76,32 @@ void Item::Move(const int playerHp,const DirectX::XMFLOAT3& distance)
 		speed = +1.5f;
 	}*/
 
+	//速度を決まる
+	move.x = speed + speedBoost;
+	//speedがspeedLimにならないように
+	if (speed > speedLim) {
+		speed = speedLim;
+	}
+	if (speed < speedLim)
+	{
+		speed += 0.001f;
+	}
+	
+	position_.x += move.x * 1.8f;
+	timer--;
+	if (timer >= 0) {
+		position_.x = distance.x + 60;
+	}
+
+	int r = rand() % 20 - 10;
+	int r2 = rand() % 11 + 1;
 	//プレイヤーを通り過ぎると前方に移動する
-	if (position_.x <= distance.x-20 ) {
-		position_ = { distance.x+50,distance.y,distance.z };
+	if (position_.x <= distance.x-60 ) {
+		position_ = { distance.x + 60,distance.y + r,distance.z };
+		if (position_.y <= -20) {
+			position_.y=distance.y + r2;
+			scale_.y = 5.0f;
+		}
 		ChangMode(playerHp);
 	}
 	ItemObj_->SetPosition(position_);
@@ -87,20 +110,7 @@ void Item::Move(const int playerHp,const DirectX::XMFLOAT3& distance)
 
 void Item::ChangMode(const int playerHp)
 {
-	////プレイ状況で必要な効果に準ずるモードチェンジ
-	////現状は仮で順番に変更される
-	//if (isDamageBoost == true) {
-	//	isDamageBoost = false;
-	//	isHeel = true;
-	//}
-	//else if (isHeel == true) {
-	//	isHeel = false;
-	//	isSlow = true;
-	//}
-	//else if (isSlow == true) {
-	//	isSlow = false;
-	//	isDamageBoost = true;
-	//}
+
 	
 
 	//プレイヤーがの体力が少ないとき
@@ -119,21 +129,31 @@ void Item::ChangMode(const int playerHp)
 	while (true) {
 		int r = rand() % 100 + 1;
 		int r2 = rand() % 100 + 1;
-		if (heelPercent >= r) {
+		int r3 = rand() % 100 + 1;
+		if (heelPercent >= r) {//回復確率
 			isSlow = false;
 			isDamageBoost = false;
 			isHeel = true;
+			timer = popCoolTime * playerHp;
 		}
 		else {
 			if (slowPercent >= r2) {
 				isHeel = false;
 				isDamageBoost = false;
 				isSlow = true;
+				timer = popCoolTime * playerHp;
 			}
 			else {
-				isSlow = false;
-				isHeel = false;
-				isDamageBoost = true;
+				if (damagePercent>= r3) {
+					isSlow = false;
+					isHeel = false;
+					isDamageBoost = true;
+					timer = popCoolTime * playerHp;
+				}
+				else {
+					//追加があれば
+					timer = popCoolTime * playerHp * 2;
+				}
 			}
 		}
 		break;
@@ -146,15 +166,16 @@ void Item::Ability(int playerHp,int bossHp, const DirectX::XMFLOAT3& distance)
 		bossHp -= 1;
 	}
 	else if (isHeel == true) {
-		int maxHp = 3;
-		if (maxHp > playerHp) {
+		int maxHp = 5;
+		if (maxHp >= playerHp) {
 			playerHp += 1;
 		}
 	}
 	else if (isSlow == true) {
 
 	}
-	position_ = { distance.x + 50,distance.y,distance.z };
+	int r = rand() % 20 - 10;
+	position_ = { distance.x + 60,distance.y + r,distance.z };
 	ChangMode(playerHp);
 	ItemObj_->SetPosition(position_);
 }
